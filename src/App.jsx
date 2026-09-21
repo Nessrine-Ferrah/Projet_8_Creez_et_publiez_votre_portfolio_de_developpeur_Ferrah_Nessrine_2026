5f47734c75d91ac3db288574ab1913e3bac21787
Ferrah Nessrine <nessrineferrah01@hotmail.com>
2026-09-21 17:25:44 +0200

    Importée le composant About.jsx dans App.jsx pour compléter la présentation globale.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navigation from './components/Navigation'
 import Header from './components/Header/Header'
-
+import About from './components/About'
 
 function App() {
   return (
     <>
       <Navigation />
       <Header />
+      <About />
     </>
   )
 }
