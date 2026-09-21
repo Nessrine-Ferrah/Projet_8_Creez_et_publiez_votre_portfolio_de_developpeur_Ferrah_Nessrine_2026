4134c64921bd5a10298e9da529c894c79dcb9f17
Ferrah Nessrine <nessrineferrah01@hotmail.com>
2026-09-21 17:06:40 +0200

    Création du composant Badge réutilisable : ajout du composant Badge avec prop `children` permettant d'afficher un contenu différent selon le composant où il est utilisé et intégration du Badge dans les composants Header et ProjectCard pour une réutilisation cohérente dans l'interface.

---
+++ src/components/Badge.jsx
@@ -0,0 +1,8 @@
+function Badge ({children}) {
+    return (
+        <span className="inline-block px-4 py-2 bg-primary text-text-button font-semibold rounded-3xl">
+            {children}
+        </span>
+    )
+}
+export default Badge
